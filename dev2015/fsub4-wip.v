3492435ab4de952966474a2b4a72c393aa080ddd
Tiark Rompf <tiark.rompf@epfl.ch>
2015-10-27 23:23:38 +0100

    sstpd version

--- dev2015/fsub4-wip.v
+++ dev2015/fsub4-wip.v
@@ -2083,6 +2083,22 @@ Proof.
   apply stp2_extendH_mult0; assumption.
 Qed.
 
+Lemma sstpd2_extendS : forall x v1 G1 G2 T1 T2 STO H m,
+                       sstpd2 m G1 T1 G2 T2 STO H ->
+                       sstpd2 m G1 T1 G2 T2 ((x,v1)::STO) H.
+Proof.
+  intros. inversion H0 as [n1 Hsub]. exists n1.
+  apply stp2_extendS; assumption.
+Qed.
+
+Lemma sstpd2_extendS_mult : forall G1 G2 T1 T2 STO STO2 H m,
+                       sstpd2 m G1 T1 G2 T2 STO H->
+                       sstpd2 m G1 T1 G2 T2 (STO2++STO) H.
+Proof.
+  intros. inversion H0 as [n1 Hsub]. exists n1.
+  apply stp2_extendS_mult; assumption.
+Qed.
+
 Lemma sstpd2_reg2 : forall G1 G2 T1 T2 STO H m,
                        sstpd2 m G1 T1 G2 T2 STO H ->
                        sstpd2 true G2 T2 G2 T2 STO H.
