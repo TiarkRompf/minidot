87b4cdd8f80238650171aeb85897c8792f1ce5a1
Tiark Rompf <tiark.rompf@epfl.ch>
2017-03-19 23:54:34 -0400

    simpler model

--- dev2015/nano_total_withStore.v
+++ dev2015/nano_total_withStore.v
@@ -20,6 +20,7 @@ Inductive tm : Type :=
   | ttrue : tm
   | tfalse : tm
   | tvar : id -> tm
+  | tref : tm -> tm
   | tapp : tm -> tm -> tm (* f(x) *)
   | tabs : tm -> tm (* \f x.y *)
 .
@@ -27,19 +28,23 @@ Inductive tm : Type :=
 
 Inductive vl : Type :=
 | vbool : bool -> vl
-| vabs  : list nat -> tm -> vl
+| vabs  : list vl -> tm -> vl
+| vref  : nat -> vl
 .
 
 
 Inductive ty : Type :=
   | TBool  : ty
   | TFun   : ty -> ty -> ty
+  | TRef   : ty -> ty
 .
 
+Definition venv := list vl.
+Definition tenv := list ty.
 Definition store := list vl.
 Definition Gamma := list ty.
 Definition rho   := list nat.
-Definition location : Type := nat.
+Definition location := nat.
 
 
 Hint Unfold store.
@@ -75,6 +80,9 @@ Inductive has_type : Gamma -> tm -> ty -> Prop :=
 | t_var: forall x env T1,
            index x env = Some T1 ->
            has_type env (tvar x) T1
+| t_ref: forall e env T1,
+           has_type env e T1 ->
+           has_type env (tref e) (TRef T1)
 | t_app: forall env f x T1 T2,
            has_type env f (TFun T1 T2) ->
            has_type env x T1 ->
@@ -97,7 +105,7 @@ Some (Some v))   means result v
 Fixpoint itype (k:nat) : Type := 
  match k with 
   | 0    => unit
-  | S k' => prodT (itype k') ((list (itype k')) -> store -> location -> Prop)
+  | S k' => prodT (itype k') ((list (itype k')) -> store -> vl -> Prop)
  end.
 
 
@@ -107,10 +115,7 @@ Definition type  : Type := forall (k : nat), (itype k).
 
 
 (* Definition : belong to a type *)
-
-
-
-Definition beIn (j k:nat) : Type := (lt j k) -> (imemtype j * store * location) -> (itype k) -> Prop .
+Definition beIn (j k:nat) : Type := (lt j k) -> (imemtype j * store * vl) -> (itype k) -> Prop .
 
 Lemma xx: forall j, j < 0 -> False. intros. omega. Defined.
 Lemma yy: forall j k, j < S k -> ~ j < k -> j = k. intros. omega. Qed.
@@ -127,39 +132,10 @@ Proof.
 Defined.
 
 
-Print In.
-
-Definition In2 j k : beIn j k := 
-  nat_rect
-    (fun k0 : nat => forall j0 : nat, beIn j0 k0)
-  (fun (j0 : nat) (H : j0 < 0) (_ : imemtype j0 * store * location)
-     (_ : itype 0) => (fun H0 : False => False_rect Prop H0) (xx j0 H))
-  (fun (k0 : nat) (IHk : forall j0 : nat, beIn j0 k0) 
-     (j0 : nat) (H : j0 < S k0) (X : imemtype j0 * store * location)
-     (X0 : itype (S k0)) =>
-   let s := lt_dec j0 k0 in
-   match s with
-   | left l => IHk j0 l X (fst X0)
-   | right n =>
-       (fun H0 : j0 = k0 =>
-        eq_rect_r
-          (fun j1 : nat =>
-           j1 < S k0 -> imemtype j1 * store * location -> ~ j1 < k0 -> Prop)
-          (fun (_ : k0 < S k0) (X1 : imemtype k0 * store * location)
-             (_ : ~ k0 < k0) =>
-           snd X0
-             ((fun H2 : imemtype k0 => H2) (let (H2, _) := fst X1 in H2))
-             ((fun H2 : store => H2) (let (_, H2) := fst X1 in H2))
-             ((fun H2 : location => H2) (let (_, H2) := X1 in H2))) H0 H X n)
-         (yy j0 k0 H n)
-   end) k j.
-
-
-
 (*Definition 1 Approx*)
-Definition itype_approx (j k : nat) : Type := (lt j k) -> (itype k) -> ( (itype j)).
+Definition itype_approx (j k : nat) : Type := lt j k -> itype k -> itype j.
 
-Definition imemtype_approx (j k : nat) : Type := (lt j k) -> (imemtype k) -> (imemtype j).
+Definition imemtype_approx (j k : nat) : Type := lt j k -> imemtype k -> imemtype j.
 
 Lemma ApproxT j k: itype_approx j k.
 Proof.
@@ -173,7 +149,7 @@ Proof.
 Defined.
 
 
-Definition Approx j k : imemtype_approx j k :=
+Definition ApproxMem j k : imemtype_approx j k :=
   fun (a : (lt j k)) (psi_k : imemtype k) => 
     map (ApproxT j k a) psi_k
 .
@@ -182,105 +158,80 @@ Definition Approx j k : imemtype_approx j k :=
 Definition imemtype_sat (k: nat) (psi: imemtype k) (M: store) : Prop :=
   length psi = length M /\ 
   (forall j l (a : lt j k), l < (length psi) -> 
-    match index l psi with
-    | Some tau => In j k a (Approx j k a psi, M, l) tau
-    | _ => False
-    end
-  )  
-.
-
-(*
-Definition imemtype_sat (k:nat) : Type := (imemtype k) -> store -> Prop.
-
-Definition SAT k : imemtype_sat k :=
-fun (psi : imemtype k) (M : store) => 
-  (length psi) <= (length M) 
-/\
-  ( forall j l (a : (lt j k)), l < length (psi) ->
-    match (index l psi) with
-    | Some tau => In j k a (Approx j k a psi, M, l) (tau) 
-    | _ => True
+    match index l psi, index l M  with
+    | Some tau, Some v => In j k a (ApproxMem j k a psi, M, v) tau
+    | _,_ => False
     end
   )
-.*)
+.
+
+
 
 (* Definition 3 Memory Extension *)
-Definition MemoryEx (k: nat)(psi1 : imemtype k)(M1 : store) (psi2: imemtype k) (M2 : store) : Prop :=
+Definition MemoryEx (k: nat)(psi1 : imemtype k)(M1 : store)(psi2: imemtype k) (M2 : store) : Prop :=
   imemtype_sat k psi1 M1 /\ imemtype_sat k psi2 M2 /\
   forall l, l < length psi1 -> index l psi1 = index l psi2 /\ index l M1 = index l M2
 .
 
-(*
-Definition MemoryEx j k : Type := (le j k) -> (imemtype k * store) -> (imemtype j * store) -> Prop.
+Definition MemoryExLt (k: nat)(psi1 : imemtype k)(M1 : store)(j: nat)(psi2: imemtype j) (M2 : store) : Prop :=
+  forall (a: j < k),
+  imemtype_sat j (ApproxMem j k a psi1) M1 /\ imemtype_sat j psi2 M2 /\
+  forall l, l < length psi1 -> index l (ApproxMem j k a psi1) = index l psi2 /\ index l M1 = index l M2
+.
 
-Lemma ME j k : MemoryEx j k.
-Proof. 
-  destruct (lt_dec j k). (* j < k *)
-    unfold MemoryEx. intros. generalize dependent j. revert X.
-    induction k. intros. omega.
-    intros. destruct X. destruct X0.
-    eapply ( (forall x, x < length (i) -> 
-              (index x i0) = (index x (Approx j (S k) l i))) /\
-             (SAT j i0 s0) ).
-  unfold MemoryEx. intro. assert (j = k). omega. subst j.
-  intros. destruct X. destruct X0.
-  eapply ( (forall x, x < length (i) -> (index x i0) = (index x i)) /\
-           (SAT k i0 s0) ).
-Qed.  
-*)
+(* Note: def from paper requires j <= k. Not sure if needed here. *)
+
+
+Lemma MemoryEx_refl: forall k psi M, MemoryEx k psi M psi M.
+Proof.
+  admit.
+Qed.
+
+
 
 
 (* Definition 4 Type *)
 
 Definition typeP (k :nat)(tau : itype k) : Prop := 
-forall j (psi: imemtype j) M l (a : lt j k), In j k a (psi, M, l) tau ->
+forall j (psi: imemtype j) M v (a : lt j k), In j k a (psi, M, v) tau ->
   imemtype_sat j psi M /\ 
   forall (psi2: imemtype j) (M2 : store), 
-    MemoryEx j psi M psi2 M2 -> In j k a (psi2, M2, l) tau 
+    MemoryEx j psi M psi2 M2 -> In j k a (psi2, M2, v) tau 
 .
 
-(*
-Lemma tyP k : typeP k.
-Proof. 
-  unfold typeP. intros.
-  apply 
-  ( (SAT k X H) /\ 
-    forall t (tau : itype t)(a : lt k t)(j :nat)
-           (psi_j :imemtype j)(M' : store)(b: le j k)(c: lt j t), 
-      In k t a (X, H, H0) tau ->
-      ME j k b (X, H) (psi_j, M') ->
-      In j t c (psi_j, M', H0) tau
-  ).
-Qed.
-*)
 
 
-Fixpoint teval(n: nat)(M: store)(env: rho)(t: tm){struct n}: (store * nat * (option (option location))):=
+
+Fixpoint teval(n: nat)(M: store)(env: venv)(t: tm){struct n}: (store * nat * (option (option vl))):=
   match n with
     | 0 => (M, 0, None)
     | S n =>
       match t with
-        | ttrue        => ((vbool true)  :: M, 1, Some (Some (length M)) )
-        | tfalse       => ((vbool false) :: M, 1, Some (Some (length M)) ) 
+        | ttrue        => (M, 1, Some (Some (vbool true)) )
+        | tfalse       => (M, 1, Some (Some (vbool false)) ) 
         | tvar x       => (M, 1, Some (index x env))
+        | tref ex       =>
+          match teval n M env ex with
+            | (M1, de, None)           => (M1,     1+de, None)
+            | (M1, de, Some None)      => (M1,     1+de, Some None)
+            | (M1, de, Some (Some vx)) => (vx::M1, 1+de, Some (Some (vref (length M1))))
+          end
 (*        | ttyp T       => (((vty env T) :: M), 1, Some (Some (length M)))*)
-        | tabs y       => ((vabs env y)  :: M, 1, Some (Some (length M)) )
+        | tabs y       => (M, 1, Some (Some (vabs env y)) )
         | tapp ef ex   =>
           match teval n M env ex with
-            | (M1, k1, None) => (M1, k1 + 1, None)
-            | (M1, k1, Some None) => (M1, k1 + 1, Some None)
-            | (M1, k1, Some (Some vx_loc)) =>
-              match teval (n - k1) M1 env ef with
-                | (M2, k2, None) => (M2, k1 + k2 + 1, None)
-                | (M2, k2, Some None) => (M2, k1 + k2 + 1, Some None)
-                | (M2, k2, Some (Some (vf_loc))) =>
-                  match (index vf_loc M2) with 
-                    | None => (M2, k1 + k2 + 1, Some None)
-(*                    | Some (vty a b) => (M2, k1 + k2 + 1, Some None)*)
-                    | Some (vbool _) => (M2, k1 + k2 + 1, Some None)
-                    | Some (vabs env2 ey) => 
-                      teval (n - k1 - k2) M2 (vx_loc::env2) ey 
-                  end 
+            | (M1, df, None)           => (M1, 1+df, None)
+            | (M1, df, Some None)      => (M1, 1+df, Some None)
+            | (M1, df, Some (Some vx)) =>
+              match teval (n - df) M1 env ef with
+                | (M2, dx, None)                       => (M2, 1+df+dx, None)
+                | (M2, dx, Some None)                  => (M2, 1+df+dx, Some None)
+                | (M2, dx, Some (Some (vbool _)))      => (M2, 1+df+dx, Some None)
+                | (M2, dx, Some (Some (vref _)))       => (M2, 1+df+dx, Some None)
+                | (M2, dx, Some (Some (vabs env2 ey))) => 
+                  match teval (n - df - dx) M2 (vx::env2) ey with
+                    | (M3, dy,  res)                 => (M3, 1+df+dx+dy, res)
+                  end
               end
           end
       end
@@ -422,7 +373,7 @@ forall n, n > nm -> exists k, teval n env e = (k, Some (Some v)).
 
 
 (* Defitition 5 : Expr :Type *)
-Definition ExpT (k t:nat)(a : lt k t) (e: tm) (R: rho) (psi: imemtype k) (M : store) (tau: itype t): Prop := 
+Definition ExpT (k t:nat)(a : lt k t) (e: tm) (R: venv) (psi: imemtype k) (M : store) (tau: itype t): Prop := 
   exists j M' psi' l, tevaln M R e l M' j /\
                       MemoryEx k psi M psi' M' /\
                       In k t a (psi', M', l) tau
@@ -437,84 +388,111 @@ Definition ExpT (k t: nat)(a : lt k t) (e :tm) (H : rho) (psi : imemtype k) (M :
 *)
 
 
+(*
+Fixpoint Bool k: itype k :=
+  match k return itype k with
+    | 0 => tt
+    | S k' => (Bool k', fun P M v =>
+                          (imemtype_sat k' P M) /\ (v = vbool true \/ v = vbool false)
+              )
+  end.
+
+(imemtype_sat k X H) /\ 
+    (index X0 H = Some (vbool true) \/ index X0 H = Some (vbool false)).
+*)
+
 Lemma Bool k : itype k.
 Proof. induction k. apply tt.
   simpl. split. apply IHk.
-  intros. apply ( 
-    (imemtype_sat k X H) /\ 
-    (index X0 H = Some (vbool true) \/ index X0 H = Some (vbool false))
-  ).
+  intros psi M v. apply ( 
+    (imemtype_sat k psi M) /\ 
+    (v = vbool true \/ v = vbool false)).
 Defined.
 
 Lemma Fun (T1 T2 : type) k : itype k.
 Proof. induction k. apply tt.
   simpl. split. apply IHk.
-  intros. apply (
-    (imemtype_sat k X H) /\ exists env2 e, 
-             index X0 H = Some (vabs env2 e) /\
-             (forall (psi' : imemtype k) (M' : store) (l : location)(c: lt k (S k)), 
-                ((MemoryEx k X H psi' M')) /\ (In k (S k) c (psi', M', l) (T1 (S k)))
-             -> (ExpT k (S k) c e (l::env2) psi' M' (T2 (S k)) )) ).
-Qed.
-
-(* FIXME: this will probably need to be a fixpoint *)
-Inductive Match {k} : ty -> itype k -> Prop :=
-  | Mtop: Match TBool (Bool k) 
-  | Mall: forall T1 T2 tau1 tau2,
-          Match T1 (tau1 k) -> 
-          Match T2 (tau2 k) ->
-          Match (TFun T1 T2) ((Fun tau1 tau2) k)
-.
+  intros psi M v. apply (
+    (imemtype_sat k psi M) /\ exists env2 e, 
+             v = (vabs env2 e) /\
+             (forall (psi' : imemtype k) (M' : store) (v : vl)(c: lt k (S k)), 
+                ((MemoryEx k psi M psi' M')) /\ (In k (S k) c (psi', M', v) (T1 (S k)))
+             -> (ExpT k (S k) c e (v::env2) psi' M' (T2 (S k)) )) ).
+Defined.
 
 
-Definition val_type k (psi : imemtype k) (M: store) (T : ty) (l: location) : Prop := 
-  forall (tau: type), Match T (tau k) ->
-  forall a : (lt k (S k)), In k (S k) a (psi, M, l) (tau (S k)).
-
-(*
-Fixpoint val_type k (psi : imemtype k) (M: store) (T : ty) (l: location) : Prop := 
-  match T with 
-  | TBool      => forall a : (lt k (S k)), 
-                  In k (S k) a (psi, M, l) (Bool (S k))
-  | TFun T1 T2 => forall a : (lt k (S k)) (tau1 tau2 : type),
-                  val_type k psi M T1 l 
+Fixpoint val_type (T:ty): type :=
+ match T with
+    | TBool => Bool
+    | TFun T1 T2 => Fun (val_type T1) (val_type T2)
+    | TRef T => Bool (* TODO *)
+  end.
 
 
+Lemma zz k: k < (S k). Proof. auto. Defined.
 
-
-Fixpoint val_type nm (v:vl) (T:ty) : Prop := match v, T with
-| vbool b, TBool => True
-| vabs env y, TFun T1 T2 =>
-  (forall vx k, k <= nm /\ val_type k vx T1 ->
-     exists v, tevaln (vx::env) y v /\ val_type k v T2) 
-| _,_ => False
-end
-.
-*)
-
-Definition WellFormed (k :nat)(G: Gamma) (R: rho) (M: store) (psi: imemtype k) : Prop :=
-  imemtype_sat k psi M /\
+Definition wf_env (G: Gamma) (H: venv) : Prop :=
   forall x T, index x G = Some T ->
-  ( exists loc tau, index x R = Some loc /\ index loc psi = Some tau /\ Match T tau
-  )
-.
+              exists v, index x H = Some v /\
+                        forall k psi M,
+                          imemtype_sat k psi M -> 
+                          In k (S k) (zz k) (psi,M,v) (val_type T (S k)).
 
-(*Lemma EnvExt: forall M psi k j (a : lt j k), 
-  imemtype_sat k psi M -> In j k a (psi_j, M, 
+
+(* simplify a goal like:
+     In k (S k) (zz k) (psi, M, vbool true) (val_type TBool (S k))
 *)
+Ltac In_simpl k:= simpl; destruct (lt_dec k k); try omega;
+                  unfold eq_rect_r; unfold eq_rect; unfold eq_sym;
+                  match goal with
+                      H: ~ k < k |- _ => remember (yy k k (zz k) H) as V
+                  end;
+                  match goal with
+                      H: ?V = (yy k k (zz k) _) |- _ => clear H; destruct V
+                  end; simpl.
 
-(* FIXME: Match will need to be part of conclusion *)
-Theorem full_total_safety : forall e G T k,
-  has_type G e T -> forall rho M psi, WellFormed k G rho M psi -> forall tau, Match T (tau k)-> 
-  exists t a, ExpT k t a e rho psi M (tau t).
-Proof. induction e; intros; inversion H; subst.
-  - Case "true". exists (S k). assert (k < (S k)) by omega. exists H2.
+
+Theorem full_total_safety : forall e G T,
+  has_type G e T -> forall H M, wf_env G H ->
+                                forall k psi,
+                                        imemtype_sat k psi M ->
+                                        exists n n' M' v,
+    teval n M H e = (M', n', Some (Some v)) /\
+      exists psi', MemoryEx k psi M psi' M' /\ In k (S k) (zz k) (psi',M',v) (val_type T (S k)).
+Proof.
+  induction e; intros; inversion H; subst.
+  - Case "true".
+    exists 1. exists 1. exists M. exists (vbool true). split. reflexivity.
+    intros. exists psi. split. eapply MemoryEx_refl.
+    In_simpl k. eauto. 
+  - Case "false".
+    exists 1. exists 1. exists M. exists (vbool false). split. reflexivity.
+    intros. exists psi. split. eapply MemoryEx_refl.
+    In_simpl k. eauto. 
+  - Case "var".
+    specialize (H1 _ _ H5). destruct H1 as [v [? ?]]. 
+    exists 1. exists 1. exists M. exists v. split. simpl. rewrite H1. reflexivity.
+    intros. specialize (H3 k psi M H2). exists psi. split. eapply MemoryEx_refl. assumption.
+  - Case "ref". 
+    specialize (IHe _ _ H5 _ M H1 k psi).
+    destruct IHe as [n [n' [M' [v [E F]]]]].
+    assumption.
+    exists (S n). exists (S n'). exists (v::M'). exists (vref (length M')).
+    split. simpl. rewrite E. reflexivity.
+    intros. (* specialize (F k psi H2). *)
+
+    destruct F as [psi' [ME IN]].
+    exists (val_type T1 k::psi'). split.
+    admit. (* extend MemoryEx *)
+    admit. (* In val_type TRef *)
+  - Case "app".
+    destruct (IHe1 G _ H6 H0 M H1 k psi H2) as [nf [nf' [MF' [vf [EF [psif' [MEF INF]]]]]]].
+    assert (imemtype_sat k psif' MF'). admit. (* get from INF *)
+    destruct (IHe2 G _ H8 H0 MF' H1 k psif' H3) as [nx [nx' [MX' [vx [EX FX]]]]]. 
+    (* apply function val ... *)
     
-    unfold ExpT. exists 1. exists ((vbool true)::M). exists ((Bool k)::psi). exists (length M).
-    (* tevaln *) split. exists 1. simpl. reflexivity.
-    (* MemoryEx *) split. unfold MemoryEx. unfold WellFormed in H0. destruct H0. split. assumption.
-    split.
-
+   (* WORK IN PROGRESS, UP TO HERE ... *)
+    
     {
     (* TODO: generalize and pull this out as a proper lemma with induction on k *)
     
